a84c13a002da49dce7e7ece06212e70772205bb7
sanket kulkarni <sanket kulkarni@DESKTOP-I86G2PA>
2019-12-13 13:13:49 +0530

    First commit

---
+++ collectionDemo/src/main/java/com/scp/java/collectionDemo/Appstart.java
@@ -0,0 +1,24 @@
+package com.scp.java.collectionDemo;
+
+import java.util.HashMap;
+
+public class Appstart {
+public static void main(String[] args) {
+	
+	HashMap<String, String> hmap= new HashMap<String, String>();
+	hmap.put("1", "V1");
+	hmap.put("2", "V2");
+	hmap.put("3", "V3");
+	hmap.put("4", "V4");
+	System.out.println(hmap);
+	ABC abc= new ABC();
+	System.out.println(abc.hashCode());
+	System.out.println(abc);
+	
+}
+}
+class ABC{
+	private int no;
+}
+
+
